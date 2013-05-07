c6f09234b0ab96ccd55bbc2ed0b7949c801e3caa
ku@digitaldolphins.jp <ku@digitaldolphins.jp@83bbfd6a-ce81-11de-be1b-2fb949943840>
2013-05-07 07:25:48 +0000

    1.0.17 ・Adobe Reader 11.xに対応。

--- chkiearea/Properties/AssemblyInfo.cs
+++ chkiearea/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ChkIEArea")]
-[assembly: AssemblyCopyright("Copyright (C)  2008,2012")]
+[assembly: AssemblyCopyright("Copyright (C)  2008,2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -30,4 +30,4 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.16.0")] // HERE
+[assembly: AssemblyFileVersion("1.0.17.0")] // HERE
